902fb75554cc7fc5e2aff811949599c2c331bed2
warranty99 <146296228+warranty99@users.noreply.github.com>
2024-02-02 13:42:07 -0300

    Create SOG+.py

---
+++ SOG+/SOG+.py
@@ -0,0 +1,111 @@
+import random
+import re
+import time # Note To Readers: Don't write your interpreters in python... Dont write anything in python that requires doing this.. Don't write this, just don't, use ASM to do shit like this....
+class gvar:
+    definer = r"\[(.*?)\]"
+    string = r"\"(.*?)\""
+    encapsulator = r"\{(.*?)\}"
+    FuncParameters = r"\((.*?)\)"
+    varDefiner = r"([^=]+)=(.+)"
+    varGreaterThan = r"([^>]+)>(.+)"
+    varLessThan = r"([^<]+)<(.+)"
+    varIs = r"([^=]+)==(.+)"
+    commaSeparator = r"([^=]+),(.+)"
+    UVars = {}
+    UFuncs = {}
+    line=1
+    class IMPORTS:
+        class COLOR:
+            RED = "\033[91m"
+            GREEN = "\033[92m"
+            BLUE = "\033[94m"
+            RESET = "\033[0m"
+            IS = False
+        class THIS:
+            SOG = ":3 why you so so soggy"
+            IS = False
+        class MATH:
+            IS = False  
+def interpret(line):
+    if r"func[" in line:
+        function_name = re.search(gvar.definer, line) 
+        if function_name is not None:
+            function_name = re.search(gvar.definer, line).group(1)
+            function_params = re.search(gvar.FuncParameters, line)
+            if function_params is not None:
+                function_params = re.search(gvar.FuncParameters, line).group(1)
+                function_code = re.search(gvar.encapsulator, line)
+                if function_code is not None:
+                    function_code = re.search(gvar.encapsulator, line).group(1)
+                    gvar.UFuncs[function_name] = function_code
+    elif r"if[" in line:
+        match = re.search(gvar.definer, line)        
+        if match is not None:
+            match = re.search(gvar.definer, line).group(1)
+            match = re.search(gvar.varIs, match)
+            if match is not None: # If it is i == 1, for example.
+                arg1 = match.group(1)
+                arg2 = match.group(2)
+                if arg1 is not int:
+                    if gvar.UVars.get(arg1) == arg2:
+                        encapsulated = re.search(gvar.encapsulator, line)
+                        if encapsulated is not None:
+                            interpret(encapsulated.group(1))
+                    else:        
+                        pass        
+        else:
+            print("Line", gvar.line, "> Syntax error!, Missing Closing Bracket! -> ]")               
+    elif r"print[" in line:
+        match = re.search(gvar.definer, line)
+        if match is not None:
+            if re.search(gvar.string, line) == None:
+                if gvar.UVars.get(match) == None:
+                    print(gvar.UVars.get(match.group(1)))
+                else:    
+                    print("Line", gvar.line, "> Syntax error!, Nothing provided for function 'print'!")
+            else:    
+                print(str(re.search(gvar.string, line).group(1)))
+        elif match == None: 
+            print("Line", gvar.line, "> Syntax error!, Nothing provided for function 'print'!")
+        else:
+            print("Line", gvar.line, "> Syntax error!, Missing Closing Bracket! -> ]")
+    elif r"var[" in line:
+        match = re.search(gvar.definer, line)
+        if match is not None:
+            match_text = match.group(1)
+            var_match = re.search(gvar.varDefiner, match_text)
+            if var_match is not None:
+                gvar.UVars[var_match.group(1)] = var_match.group(2)
+            else:
+                print("Line", gvar.line, "> Syntax error!, Invalid variable definition!")
+        else:
+            print("Line", gvar.line, "> Syntax error!, Missing Closing Bracket! -> ]")
+    elif r"import[" in line:
+        if match is not None:
+            match = re.search(gvar.definer, line).group(1)
+            if match == "COLOR":
+                gvar.IMPORTS.COLOR.IS=True
+            elif match == "MATH":
+                gvar.IMPORTS.MATH.IS=True
+                import math    
+        elif match == None:    
+            print("Line", gvar.line, "> Syntax error!, Nothing provided for function 'import'!")
+        else:
+            if "]" in line:
+                print("Line", gvar.line, "> Import error!. Import not recognized!")
+            else:
+                print("Line", gvar.line, "> Syntax error!, Missing Closing Bracket! -> ]")
+    elif r"pause[" in line:
+        match = re.search(gvar.definer, line)
+        if match is not None:
+            time.sleep(int(re.search(gvar.definer, line).group(0)))
+        elif match is None:
+            print("Line", gvar.line, "> Syntax error!, Missing Closing Bracket! -> ]")
+    else:
+        if gvar.UFuncs.get(line) is not None:
+            interpret(gvar.UFuncs.get(line))
+        else:
+            print("OBJ not recognized! make sure to use brackets! -> []")    
+while True:
+    cmd = input("sog >")
+    interpret(cmd)
